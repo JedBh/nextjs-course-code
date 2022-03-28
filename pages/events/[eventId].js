b7ed38e98f3114735cefa3842d774431aa3ec945
JedBh <jbenhod@gmail.com>
2022-03-28 16:59:05 +0300

    add: event-detail, [eventId].js file got some logic

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,37 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 function EventDetailPage() {
+  const router = useRouter();
+  const id = router.query.eventId;
+  const event = getEventById(id);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  const { title, description, location, date, image } = event;
+
+  console.log(image);
+
   return (
-    <div>
-      <h1>Event Details</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={title} />
+      <EventLogistics
+        date={date}
+        address={location}
+        image={image}
+        imageAlt={title}
+      />
+      <EventContent>
+        <p>{description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
 
