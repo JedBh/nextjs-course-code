45130b45f54c11b97415d8aaa34b68cab2eaa8b9
JedBh <jbenhod@gmail.com>
2022-03-28 16:22:45 +0300

    add: event item styles and jsx

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/EventList";
+
+function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <EventList items={featuredEvents} />
+    </div>
+  );
+}
+
+export default HomePage;
