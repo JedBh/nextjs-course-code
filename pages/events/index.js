adb9f7b86cfc90894c10d5e502d59a32ea762e52
unknown <jbenhod@gmail.com>
2022-03-31 11:04:39 +0300

    add: events-search

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,16 @@
 import { getAllEvents } from '../../dummy-data'
 import EventList from '../../components/events/EventList'
+import EventSearch from '../../components/events/events-search'
+import { Fragment } from 'react/cjs/react.production.min'
 
 function AllEventsPage() {
   const events = getAllEvents()
 
   return (
-    <div>
+    <Fragment>
+      <EventSearch />
       <EventList items={events} />
-    </div>
+    </Fragment>
   )
 }
 
